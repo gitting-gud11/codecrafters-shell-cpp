796bd2bf49e1d1f1ff1dd2157285ae5aae50bd4a
Daniel Sium <117544229+gitting-gud11@users.noreply.github.com>
2026-06-30 19:28:32 -0400

    Reworked echo function and added logic for splitting input

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <string>
 #include<sstream>
+#include<vector>
+#include<assert.h>
+
+
+std::vector<std::string> parse_input(std::string & input){
+  std::stringstream stream(input);
+
+  std::vector<std::string> tokens;
+  std::string token;
+  while(std::getline(stream,token,' ')) {tokens.push_back(token);}
+
+  return tokens;
+}
+
+std:: string echo_output(std::vector<std::string> & tokens){
+  std::string buffer;
+
+  for(size_t i=1;i<tokens.size();++i){buffer+=tokens[i];buffer.push_back(' ');}
+
+  return buffer;
+
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -13,18 +35,22 @@ int main() {
 
     std::string input;
     std::getline(std::cin,input);
-    std::stringstream stream(input);
-    std::string token, segment;
-    stream >> token >> segment;
 
-    if(token=="exit"){
+    std::vector<std::string> tokens=parse_input(input);
+
+    std::string command;
+    if(tokens.size()!=0){
+      command=tokens[0];
+    }
+
+    if(command=="exit"){
       exit(0);
     }
-    else if(token=="echo"){
-      std::cout<<segment<<"\n";
+    else if(command=="echo"){
+      std::cout<<echo_output(tokens)<<"\n";
     }
     else{
-      std::string command_failed=token+": command not found\n";
+      std::string command_failed=command+": command not found\n";
       std::cerr<<command_failed;
 
     }
