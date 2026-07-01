9bebf0194ce4393dcc0de60b2aa4ce606d4874df
Daniel Sium <117544229+gitting-gud11@users.noreply.github.com>
2026-07-01 12:33:44 -0400

    Implemented Locating Executable Files

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
+#include <cassert>
+#include <cstdlib>
+#include <filesystem>
 #include <iostream>
+#include <optional>
+#include <set>
+#include <sstream>
 #include <string>
-#include<sstream>
-#include<vector>
-#include<assert.h>
-#include<set>
+#include <vector>
 
-std::set<std::string> builtins={"echo","type","exit"};
+#ifdef _WIN32
+constexpr char os_pathsep=';';
+#else
+constexpr char os_pathsep=':';
+#endif
 
+constexpr char dir_pathsep=std::filesystem::path::preferred_separator;
 
 std::vector<std::string> parse_input(std::string & input){
   std::stringstream stream(input);
@@ -16,6 +24,48 @@ std::vector<std::string> parse_input(std::string & input){
   while(std::getline(stream,token,' ')) {tokens.push_back(token);}
 
   return tokens;
+}
+
+
+inline std::string get_command(std::vector<std::string> & tokens){
+  std::string command;
+
+  if(tokens.size()!=0){
+      command=tokens[0];
+  }
+  return command;
+}
+
+std::optional<std::string> find_path(std::string & file,std::string & path){
+
+  std::stringstream stream(path);
+  std::string buffer;
+
+  while(std::getline(stream,buffer,os_pathsep)){
+
+    if(!std::filesystem::is_directory(buffer)) continue;
+
+    std::string location=buffer+dir_pathsep+file;
+    std::filesystem::directory_entry entry{location};
+
+    if(!entry.exists()) continue;
+
+    std::filesystem::perms entry_permissions=entry.status().permissions();
+
+    std::filesystem::perms executeable=((entry_permissions & std::filesystem::perms::owner_exec) | (
+      entry_permissions & std::filesystem::perms::group_exec) | (entry_permissions & std::filesystem::perms::others_exec));
+
+      switch (executeable)
+      {
+        case (std::filesystem::perms::none):
+              break;
+        default:
+              return location;
+      }
+
+    }
+
+    return std::nullopt;
 }
 
 std:: string echo_output(std::vector<std::string> & tokens){
@@ -32,19 +82,19 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::string path=std::string(std::getenv("PATH"));
+
+  std::set<std::string> builtins={"echo","type","exit"};
 
   while(1){
     std::cout << "$ ";
 
-    std::string input;
-    std::getline(std::cin,input);
+    std::string line;
+    std::getline(std::cin,line);
 
-    std::vector<std::string> tokens=parse_input(input);
+    std::vector<std::string> tokens=parse_input(line);
 
-    std::string command;
-    if(tokens.size()!=0){
-      command=tokens[0];
-    }
+    std::string command=get_command(tokens);
 
     if(command=="exit"){
       exit(0);
@@ -60,14 +110,21 @@ int main() {
 
       if(builtins.contains(arg_type)){
         std::cout<<arg_type<<" is a shell builtin\n";
+        continue;
+      }
+
+      std::optional<std::string> returned_path=find_path(arg_type,path);
+      if(returned_path.has_value()){
+        std::cout<<arg_type<<" is "<<returned_path.value()<<"\n";
       }
       else{
-        std::cout<<arg_type<<": not found\n";
+        std::cout<<arg_type<<": command not found\n";
       }
+      
     }
     else{
       std::string command_failed=command+": command not found\n";
-      std::cerr<<command_failed;
+      std::cout<<command_failed;
 
     }
 
